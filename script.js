75b11a5e861aae04e7981e9d0826cd693b48311a
CyberBreach <shrivastavapiyush781@gmail.com>
2021-10-28 19:59:32 +0530

    Responsive cards Added

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function onload () {
-    
+const navSlide = () => {
+    const burger = document.querySelector('.burger1');
+    const nav = document.querySelector('.nav-links1');
+    const navLinks = document.querySelectorAll('.nav-links1 li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+    });
+
+
+
 }
+
+
+navSlide();
